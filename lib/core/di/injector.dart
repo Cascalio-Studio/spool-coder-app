9131e208318de407946350a284580c3435332491
Kevin Perillo <mootseeker98@gmail.com>
2025-07-30 21:55:37 +0200

    Check issue approved

--- lib/core/di/injector.dart
+++ lib/core/di/injector.dart
@@ -1,10 +1,15 @@
 import 'package:get_it/get_it.dart';
+// Import NFC platform interface and implementation
+import 'package:spool_coder_app/platform/nfc/nfc_platform.dart';
 
 /// Global service locator
 final GetIt locator = GetIt.instance;
 
 /// Register all services and repositories here
 void setupLocator() {
-  // Example:
-  // locator.registerLazySingleton<SpoolService>(() => SpoolServiceImpl());
+  // Register NFC platform implementation
+  locator.registerLazySingleton<NfcPlatformInterface>(
+    () => NfcPlatformImpl(),
+  );
+  // TODO: Register other core services (e.g., storage, logging)
 }
